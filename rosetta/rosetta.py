7cf03e7b6b7f10b3196115cc14478e8825c744b9
Zelazny7 <gravcon5@gmail.com>
2020-02-29 18:13:21 -0600

    removed dead weight and formatted

--- rosetta/rosetta.py
+++ rosetta/rosetta.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 from abc import ABC, abstractmethod
 from typing import List, Dict, Any, Type, cast
 
+
 class Operation:
     registry: Dict[str, Type[Operation]] = {}
     _indent: int = 0
@@ -9,7 +10,7 @@ class Operation:
     @staticmethod
     def register_operation(op: str, klass: Type[Operation]):
         Operation.registry[op] = klass
-    
+
     @staticmethod
     def from_dict(x: Dict[str, Any]) -> Operation:
         klass = Operation.registry.get(x["op"])
@@ -21,55 +22,54 @@ class Operation:
 
     def __init__(self, **kwargs):
         super().__init__()
-    
+
     @property
     def indent(self):
         return Operation._indent
-    
+
     @indent.setter
     def indent(self, value):
         Operation._indent = value
-    
-class SingleOperation(Operation, ABC):
-    """SingleOperation only performs one operation"""
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
+
 
 class EqualOperation(Operation):
     def __init__(self, target: str, value: Any):
         super().__init__()
         self.target = target
         self.value = value
-    
+
     def __str__(self) -> str:
         return f"{self.target} == {self.value}"
+
 
 class InOperation(Operation):
     def __init__(self, target: str, values: List[Any]):
         super().__init__()
         self.target = target
         self.values = values
-    
+
     def __str__(self) -> str:
         return f"{self.target} in ({', '.join([str(x) for x in self.values])})"
 
 
 class MultiOperation(Operation, ABC):
     """MultiOperations combine collections of SingleOperations"""
+
     def __init__(self, operands: List[Dict[str, Any]], **kwargs):
         super().__init__(**kwargs)
         self.operands = [Operation.from_dict(d) for d in operands]
 
     def __str__(self):
         self.indent += 2
-        res =  f'\n'.join([(' ' * self.indent) + str(x) for x in self.operands])
+        res = f"\n".join([(" " * self.indent) + str(x) for x in self.operands])
         self.indent -= 2
-        return res + '\n' + ' ' * self.indent + ']'
+        return res + "\n" + " " * self.indent + "]"
+
 
 class AndOperation(MultiOperation):
     def __init__(self, operands, **kwargs):
         super().__init__(operands, **kwargs)
-    
+
     def __str__(self):
         return "and [\n" + super().__str__()
 
@@ -77,70 +77,58 @@ class AndOperation(MultiOperation):
 class NotOperation(MultiOperation):
     def __init__(self, operands, **kwargs):
         super().__init__(operands, **kwargs)
-    
+
     def __str__(self):
-        return "not [\n" +  super().__str__()
+        return "not [\n" + super().__str__()
+
 
 class OrOperation(MultiOperation):
     def __init__(self, operands, **kwargs):
         super().__init__(operands, **kwargs)
-    
+
     def __str__(self):
-        return  "or [\n" +  super().__str__()
+        return "or [\n" + super().__str__()
+
 
 class AssignmentOperation(Operation):
-
     def __init__(self, variable: str, operation: Dict[str, Any], value: Any):
         super().__init__()
         self.variable = variable
         self.Operation = Operation.from_dict(operation)
         self.value = value
-    
+
     def __str__(self):
         return f"({str(self.Operation)}) => {self.variable} = {self.value}"
 
-class IfElseOperation(MultiOperation):
 
+class IfElseOperation(MultiOperation):
     def __init__(self, operands: List[Dict[str, Any]]):
         super().__init__(operands)
-    
+
     def __str__(self):
-        return  f"ifelse [\n{super().__str__()}]"
+        return f"ifelse [\n{super().__str__()}]"
 
 
 if __name__ == "__main__":
     d = {
         "op": "and",
         "operands": [
-            {
-                "op": "eq",
-                "target": "mpg",
-                "value": 25
-            },
+            {"op": "eq", "target": "mpg", "value": 25},
             {
                 "op": "not",
                 "operands": [
                     {
                         "op": "or",
                         "operands": [
-                            {
-                                "op": "eq",
-                                "target": "disp",
-                                "value": 10
-                            },
-                            {
-                                "op": "in",
-                                "target": "cyl",
-                                "values": [2,4,6,8]
-                            }
-                        ]
+                            {"op": "eq", "target": "disp", "value": 10},
+                            {"op": "in", "target": "cyl", "values": [2, 4, 6, 8]},
+                        ],
                     }
+                ],
+            },
+        ],
+    }
 
-                ]
-            }
-        ]
-        }
-    
     Operation.register_operation("and", AndOperation)
     Operation.register_operation("eq", EqualOperation)
     Operation.register_operation("not", NotOperation)
@@ -154,9 +142,9 @@ if __name__ == "__main__":
     print(res1)
 
     import json
-    with open('specs/example02.json', 'r') as fin:
+
+    with open("specs/example02.json", "r") as fin:
         j = json.load(fin)
-    
+
     res2 = Operation.from_dict(j)
     print(res2)
-    
