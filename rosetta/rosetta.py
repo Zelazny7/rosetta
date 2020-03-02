88993cd84eb01ce8af694ca26222abec605c7fcc
Graves <GravesEE@risk.regn.net>
2020-03-02 10:46:46 -0600

    decoupled operation and visitor. Now visitors are responsible for walking operations and calling methods

--- rosetta/rosetta.py
+++ rosetta/rosetta.py
@@ -2,8 +2,6 @@ from __future__ import annotations
 from abc import ABC, abstractmethod
 from typing import List, Dict, Any, Type, cast
 
-# TODO: Create StdoutTransformer instead of __str__ methods for each operation
-
 class Operation:
     registry: Dict[str, Type[Operation]] = {}
     _indent: int = 0
@@ -27,41 +25,7 @@ class Operation:
             self.operands = [Operation.from_dict(d) for d in operands]
         else:
             self.operands = []
-        # self.walk()
 
-    @property
-    def indent(self):
-        return Operation._indent
-
-    @indent.setter
-    def indent(self, value):
-        Operation._indent = value
-
-    def walk(self, other: BaseTransformer):
-        """Walk Operation and call matching methods in `other` object"""
-        klass = type(self).__name__
-
-        # call EnterAndOperation in `other` object if klass is an AndOperation and attribute exists
-        fenter = getattr(other, "Enter" + klass, None)
-        if fenter is not None:
-            fenter(self)
-
-        # Call walk on children if they exist
-        other.indent += 2
-        for i, operand in enumerate(self.operands):
-            operand.walk(other)
-
-            if i < len(self.operands):
-                fwhile = getattr(other, "While" + klass, None)
-                if fwhile is not None:
-                    fwhile(self)
-        
-        other.indent -= 2
-
-        # call ExitAndOperation in `other` object if klass is an AndOperation and attribute exists
-        fexit = getattr(other, "Exit" + klass, None)
-        if fexit is not None:
-            fexit(self)
 
 class EqualOperation(Operation):
     def __init__(self, target: str, value: Any):
@@ -69,7 +33,6 @@ class EqualOperation(Operation):
         self.target = target
         self.value = value
 
-    
 
 class InOperation(Operation):
     def __init__(self, target: str, value: List[Any]):
@@ -77,30 +40,20 @@ class InOperation(Operation):
         self.target = target
         self.value = value
 
-    
 
 class AndOperation(Operation):
     def __init__(self, operands, **kwargs):
         super().__init__(operands, **kwargs)
-
-    # def __str__(self):
-    #     return "and [\n" + super().__str__()
 
 
 class NotOperation(Operation):
     def __init__(self, operands, **kwargs):
         super().__init__(operands, **kwargs)
 
-    # def __str__(self):
-    #     return "not [\n" + super().__str__()
-
 
 class OrOperation(Operation):
     def __init__(self, operands, **kwargs):
         super().__init__(operands, **kwargs)
-
-    # def __str__(self):
-    #     return "or [\n" + super().__str__()
 
 
 class AssignmentOperation(Operation):
@@ -110,8 +63,6 @@ class AssignmentOperation(Operation):
         self.predicate = Operation.from_dict(predicate)
         self.value = value
 
-    # def __str__(self):
-    #     return f"({str(self.predicate)}) => {self.variable} = {self.value}"
 
 class IfOperation(Operation):
     def __init__(self, operands: List[Dict[str, Any]], variable: str, value: Any):
@@ -122,38 +73,64 @@ class IfOperation(Operation):
 class IfElseOperation(Operation):
     def __init__(self, operands: List[Dict[str, Any]]):
         super().__init__(operands)
-    
-class BaseTransformer(ABC):
-    def __init__(self):
-        super().__init__()
-        self.indent = 0
 
+
+def invoke(object: BaseVisitor, prefix: str, op: Operation) -> None:
+    """Check if object has `prefix` + `type(op).__name__` as an attribute and call"""
+    klass = type(op).__name__
+    func = getattr(object, prefix + klass, None)
+    if func is not None:
+        func(op)
+
+class BaseVisitor(ABC):
+    def __init__(self, indent_size=2):
+        super().__init__()
+        self._indent = 0
+        self.indent_size = indent_size
+    
     @property
     def s(self) -> str:
-        return " " * self.indent
+        """Create spaces for formatting output"""
+        return " " * self._indent
 
-class StringTransformer(BaseTransformer):
-    
+    def visit(self, op: Operation):
+        """Walk Operation and call matching methods in `other` object"""
+
+        invoke(self, "Enter", op)
+
+        # Call walk on children if they exist
+        self._indent += self.indent_size
+        for i, operand in enumerate(op.operands):
+            self.visit(operand)
+
+            if i < len(op.operands) - 1:
+                invoke(self, "While", op)
+                
+            if i == len(op.operands):
+                invoke(self, "Last", op)
+
+        self._indent -= self.indent_size
+
+        invoke(self, "Exit", op)
+
+class StringVisitor(BaseVisitor):
     def EnterEqualOperation(self, other: EqualOperation):
-        print(f"{other.target} == {other.value}", end='')
-    
+        print(f"{other.target} == {other.value}", end="")
+
     def EnterInOperation(self, other: InOperation):
         print(f"{other.target} in ({', '.join([str(x) for x in other.value])})", end="")
-    
+
     def WhileIfElseOperation(self, other: IfElseOperation):
-        print(f"\nelse ", end='')
+        print(f"\nelse ", end="")
 
     def EnterIfOperation(self, other: IfOperation):
-        print(f"if (", end='')
+        print(f"if (", end="")
 
     def ExitIfOperation(self, other: IfOperation):
-        print(f"):", end='')
-
-    
+        print(f"):", end="")
 
 
-
-class PandasTransformer(BaseTransformer):
+class PandasVisitor(BaseVisitor):
     def __init__(self, df: str, variable: str):
         super().__init__()
         self.variable = variable
@@ -165,13 +142,11 @@ class PandasTransformer(BaseTransformer):
 
     def EnterAndOperation(self, other: AndOperation):
         print(self.s + "(", end="")
-        self.indent += 2
 
     def WhileAndOperation(self, other: AndOperation):
         print(" &")
 
     def ExitAndOperation(self, other: AndOperation):
-        self.indent -= 2
         print(")", end="")
 
     def EnterEqualOperation(self, other: EqualOperation):
@@ -182,14 +157,12 @@ class PandasTransformer(BaseTransformer):
 
     def EnterOrOperation(self, other: OrOperation):
         print("(", end="")
-        self.indent += 2
 
     def WhileOrOperation(self, other: OrOperation):
         print(" |")
 
     def ExitOrOperation(self, other: OrOperation):
-        print(")")
-        self.indent -= 2
+        print(")", end="")
 
 
 if __name__ == "__main__":
@@ -227,19 +200,20 @@ if __name__ == "__main__":
     # print(res1)
 
     # demonstrate variable walking
-    v = PandasTransformer(df="df", variable="var1")
-    #res1.walk(v)
+    v = PandasVisitor(df="df", variable="var1")
 
-    v2 = StringTransformer()
-    #res1.walk(v2)
+    v.visit(res1)
+    # res1.walk(v)
 
+    # v2 = StringTransformer()
+    # res1.walk(v2)
 
     # reading from json file
-    import json
+    # import json
 
-    with open("specs/example02.json", "r") as fin:
-        j = json.load(fin)
+    # with open("specs\\example02.json", "r") as fin:
+    #     j = json.load(fin)
 
-    res2 = Operation.from_dict(j)
-    res2.walk(v2)
+    # res2 = Operation.from_dict(j)
+    # res2.walk(v2)
     # print(res2)
